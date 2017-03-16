d43562656c913fb99895affc6fb9781d85b2aff1
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-16 12:45:44 +0000

    Fill current temp div in with thermostat.currentTemp

--- src/JQueryEvents.js
+++ src/JQueryEvents.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
   $("#temperature-reset").click(function(event) {
     thermostat.reset();
     console.log("Thermostat has been reset to " + thermostat.currentTemp);
-    // alert("Thermostat has been reset to " + thermostat.currentTemp);
+    $("#current-temperature").html(thermostat.currentTemp);
   });
 
   $("#powersaving-status").click(function(event) {
     thermostat.setPowerSavingMode();
     console.log(thermostat.powerSavingMode);
-
+    $("#powersaving-status").html("PSM is " + thermostat.powerSavingMode);
   });
 
 });
