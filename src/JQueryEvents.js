1bb9e1855f325cb9bddf45dac8d76f99a4c9d2ac
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-16 13:51:06 +0000

    Receive value from selected temperature radio button

--- src/JQueryEvents.js
+++ src/JQueryEvents.js
@@ -13,4 +13,35 @@ $(document).ready(function() {
     $("#powersaving-status").html("PSM is " + thermostat.powerSavingMode);
   });
 
+  $("#set-temp").click(function() {
+    var temp = $('input[type="radio"]:checked');
+    if(temp.length > 0) {
+      $("#current-temperature").html(temp.val());
+    } else {
+      alert("No temperature selected");
+    }
+  });
+
+  // $("#set-temp").click(function(event) {
+    // $(".temp-value").each(function() {
+    //   console.log($(".temp-value").val());
+    // });
+
+
+      // if($(".temp-value").is(':checked')) {
+
+        // var temp = $(".temp-value").val();
+        // console.log(temp);
+      // };
+
+    // if($(".temp-value").is(':checked')) {
+    //   var temp = $(".temp-value").val();
+    //   console.log(temp);
+    // } else {
+    //   console.log("nothing checked");
+    // });
+    // console.log("hello");
+
+  // });
+
 });
